149a67ceef99f0433021342508b423eaea10641b
Radu Ciurlea <raduciurlea@gmail.com>
2023-05-11 22:58:20 +0200

    add basic neovim setup

---
+++ private_dot_config/nvim/lua/home/init.lua
@@ -0,0 +1,23 @@
+require "home.options"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("home/plugins")
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+        require "home.autocmds"
+        require "home.keymaps"
+    end,
+})
---
+++ private_dot_config/nvim/lua/home/plugins/init.lua
@@ -0,0 +1,48 @@
+return {
+    -- the colorscheme should be available when starting Neovim
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    {
+        "catppuccin/nvim",
+        config = function()
+            require("catppuccin").setup({})
+        end
+    },
+    { 
+        "Yazeed1s/oh-lucy.nvim",
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme oh-lucy]])
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup {
+                icons_enabled = true,
+                options = {
+                    component_separators = '', 
+                    section_separators = ''
+                },
+            }
+        end
+    },
+}
